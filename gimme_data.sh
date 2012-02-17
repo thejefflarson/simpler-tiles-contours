72ca6ba93146652f56b595e71a66d8d87c038af3
Jeff Larson <thejefflarson@gmail.com>
2012-02-17 18:57:46 -0500

    this -> sky

---
+++ gimme_data.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir data
+
+WD=`pwd`
+usgs_em () {
+  directory=$WD/$1
+  get_em $directory "http://edcftp.cr.usgs.gov/pub/data/gtopo30/global/"
+  unzip_em $directory
+  contour_em $directory
+}
+
+get_em () {
+  echo "Getting" $1 $2
+  wget -A tar.gz -m -np -nH -c -nd -P  $1 $2
+}
+
+unzip_em () {
+  cd $1
+  ls *.tar.gz | xargs -I{} tar -xzvf {}
+  cd $WD
+}
+
+contour_em () {
+  cd $1
+  rm *.shp
+  rm *.shx
+  rm *.dbf
+  for file in W140N90.DEM W100N90.DEM W060N90.DEM W140N40.DEM W100N40.DEM W060N40.DEM
+  do
+    echo "Processing $file"
+    gdal_contour $file `basename $file .DEM`.shp -i 50
+  done
+  cd $WD
+}
+
+usgs_em 'data'
