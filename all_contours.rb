3fda9af68129a645e3fa8439dfb33a99ecf0999f
Jeff Larson <thejefflarson@gmail.com>
2012-02-21 11:05:21 -0500

    logo

--- all_contours.rb
+++ all_contours.rb
@@ -5,20 +5,10 @@ ROOT = File.expand_path(File.dirname(__FILE__))
 
 map = SimplerTiles::Map.new do |m|
   m.srs     = "EPSG:3083"
-  m.bgcolor = '#aad5f2'
+  m.bgcolor = "#ffffff"
   m.width   = 423
   m.height  = 260
   m.set_bounds(-585080.885134, 6849466.721081, 4161303.603672, 9587780.816356)
-
-
-  m.layer "data/10m_coastline.shp" do |l|
-    l.query 'select * from "10m_land"' do |q|
-      q.styles 'fill' => '#ffffff',
-           'seamless' => 'true',
-             'stroke' => '#ffffff',
-             'weight' => '0.1'
-    end
-  end
 
   Dir["#{ROOT}/data/*.shp"].each do |shp|
     m.layer shp do |l|
@@ -27,6 +17,16 @@ map = SimplerTiles::Map.new do |m|
               "line-join" => "round",
                  "weight" => "0.1"
       end
+    end
+  end
+
+
+  m.layer "data/10m-coastline/10m_coastline.shp" do |l|
+    l.query 'select * from "10m_coastline"' do |q|
+      q.styles 'fill' => '#ffffff',
+           'seamless' => 'true',
+             'stroke' => '#000000',
+             'weight' => '0.1'
     end
   end
 
